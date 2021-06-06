0d3360b000dc93a9b2d207c1d75ac1be019c5ed2
helly04 <85433310+helly04@users.noreply.github.com>
2021-06-06 14:08:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+export default App;
